649fd7fd5773f3359a98b58323d98bfb45983bf5
valts08 <valts.pilans@gmail.com>
2024-07-08 18:01:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+# Foundation for "to-do list":
+# Have at least one list available to any one user ( >= 1 )
+# The lists data needs to be saved in a .txt file
+# There should be a folder for each user and an N amount of files(lists) that a user has made
+# The lists can be read and edited
+    # If the list file is empty, it will be deleted
+# Format the list (it can't be just a single continous line of text, it needs to be structured)
 
 
+class List:
+    def __init__(self):
+        pass
+
+    def read_from_list(self):
+        pass
+    
+    def edit_list(self):
+        pass
+
+class User:
+    def __init__(self):
+        self.lists = []
+        pass
 
 
 
