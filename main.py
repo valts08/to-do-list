8b3bddcfa08e3e5a1d0461f19c0e848ad517cb60
valts08 <valts.pilans@gmail.com>
2024-07-11 12:18:08 +0300

    List and User constructor update, can add list items now
    updated the List constructor to conditionally make lists, or map lists to users when loading user data
    
    can now successfully append text to existing lists

--- main.py
+++ main.py
@@ -18,30 +18,32 @@ from pathlib import Path
 # This will help go through less actions and save time(albeit very little, but we love a little challenge)
 
 class List:
-    def __init__(self, title: str, owner: str, content: str, file_path):
+    def __init__(self, title: str, owner: str, content: str, file_path: str, make_with_user: bool) -> None:
         self.owner = owner
         self.title = title
         self.location = file_path
-        # The full path to the folder of lists will be the path that is given + the name of the person who's making it
-        self.full_path = f'{self.location}\\{self.owner}'
-        list_path = f'{self.full_path}\\{self.title}'
-        # Make the directory using user given "title" string
-        # Check if the directory already exists
-        my_dir = Path(f'{self.full_path}')
+        # Check if we make a List object during User creation, or when loading existing user data
+        if make_with_user:
+            # The full path to the folder of lists will be the path that is given + the name of the person who's making it
+            self.full_path = f'{self.location}\\{self.owner}'
+            list_path = f'{self.full_path}\\{self.title}'
+            # Make the directory using user given "title" string
+            # Check if the directory already exists
+            my_dir = Path(f'{self.full_path}')
 
-        if not my_dir.is_dir():
-            os.mkdir(f'{self.full_path}')
-        
-        try:
-            # check if the list you're looking for exists
-            assert Path(f'{list_path}').is_file(), 'File path already exists'
-        except AssertionError:
-            # just add text to it, if it does exist
-            self.add_item(list_path, content)
-        else:
-            # make and open the list for writing, if it doesn't exist
-            with open(f'{list_path}.txt', "x") as file:
-                file.write(f"- {content}")
+            if not my_dir.is_dir():
+                os.mkdir(f'{self.full_path}')
+            
+            try:
+                # check if the list you're looking for exists
+                assert Path(f'{list_path}').is_file(), 'File path already exists'
+            except AssertionError:
+                # just add text to it, if it does exist
+                self.add_item(list_path, content)
+            else:
+                # make and open the list for writing, if it doesn't exist
+                with open(f'{list_path}.txt', "x") as file:
+                    file.write(f"- {content}")
 
 
     def add_item(self, list_path: str, content: str):
@@ -64,15 +66,13 @@ class List:
 
 
 class User:
-    def __init__(self, title, owner, content):
+    def __init__(self, name):
         self.user_lists = []
-        self.title = title
-        self.owner = owner
-        self.content = content
+        self.name = name
 
     def create_list(self, title, owner, content, file_path):
         # Every time a new List is made for the User instance, add it to it's list of lists
-        new_list = List(title, owner, content, file_path)
+        new_list = List(title, owner, content, file_path, make_with_user=True)
         self.user_lists.append(new_list)
 
 
@@ -99,19 +99,25 @@ def load_users(file_path):
             if folder.isalnum():
                 files_in_folder = os.listdir(f'{dir_path}\\{folder}')
                 #print(folder, 'folder')
+                user_info[folder] = User(folder)
                 for file in files_in_folder:
                     with open(f'{dir_path}\\{folder}\\{file}') as doc:
                         list_content = doc.read()
-                        user_info[folder] = User(file, folder, list_content)
+                        # We pass "file[:-4] to get rid of the ".txt" portion of the string"
+                        # because the user input is purely the name of the list e.g. "blabla", not "blabla.txt"
+                        user_info[folder].user_lists.append(List(file[:-4], folder, list_content, dir_path, make_with_user=False))
 
         return user_info
 
 if __name__ == "__main__":
     users_file_path = input("Before we start, if you have any lists already made, enter their file path here:\n")
-    existing_users = load_users(users_file_path)
-    #print(existing_users)
     print('Hello! What do you want to do next?')
     while True:
+        # Load user data every time an action is done
+        # This should be changed later on to only update 
+        # if there is any additional data the user passed
+        existing_users = load_users(users_file_path)
+        print(existing_users)
         try:
             user_input = input('Make a new list(M), Add to an existing list(A),\nRead one of your lists(R), or edit a list(E): ').upper()
             assert user_input in ['M', 'A', 'R', 'E']
@@ -128,19 +134,35 @@ if __name__ == "__main__":
 
             user_name = input('What directory(folder) name do you want your list to be under\n(a valid folder name is alphanumeric(no special characters)): ').lower()
             list_title = input('Choose what is going to be the title of your list: ').lower()
+            user_content = input("What do you want to write in your list?\n(follow the format '-item-item-item...', the dash is used as a seperator):\n")
+            full_file_path = f'{users_file_path}\\{user_name}\\{list_title}'
             match user_input:
                 case 'M':
-                    user_content = input("What do you want to write in your list?\n(follow the format '-item-item-item...', the dash is used as a seperator):\n")
                     if not Path(users_file_path).is_dir() and users_file_path[0] == '.':
+                        #  Change the 2nd part of this if statement, it doesn't make sense
                         users_file_path = os.curdir
-                    new_user = User(list_title, user_name, user_content)
+                    new_user = User(user_name)
                     new_user.create_list(list_title, user_name, user_content, users_file_path)
                 case 'A':
-                    pass
-                    print('A')
+                    # Add items to an existing list
+                    has_list = False
+                    try:
+                        # Check if file exists for given user
+                        for user_list in existing_users[user_name].user_lists:
+                            if user_list.title == list_title:
+                                has_list = True
+                                active_user_list = user_list
+                        assert has_list
+                    except AssertionError as e:
+                        print(f'List at path: {full_file_path}.txt was not found')
+                        print(e, ' Error')
+                    else:
+                        active_user_list.add_item(full_file_path, user_content)
                 case 'R':
+                    # Read content of existing list
                     print('R')
                 case 'E':
+                    # Edit an existing list
                     print('E')
         
         #os.system('cls')
